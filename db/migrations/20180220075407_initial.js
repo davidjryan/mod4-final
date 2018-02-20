59e014e7ef220feba23437d144e9066e5d27843c
David Ryan <djrdev@icloud.com>
2018-02-20 09:19:29 -0700

    migration and seed

--- db/migrations/20180220075407_initial.js
+++ db/migrations/20180220075407_initial.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable("mod4final", function(table) {
+      table.increments("id").primary();
+      table.string("title");
+
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([knex.schema.dropTable("mod4final")]);
 };
