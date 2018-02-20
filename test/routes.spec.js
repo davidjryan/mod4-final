ffabf0d059aee4aed6bc41ffefaef8f6f3d819f0
David Ryan <djrdev@icloud.com>
2018-02-20 11:09:43 -0700

    added cliient route sad path

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -20,4 +20,15 @@ describe("Client Routes", () => {
         throw error;
       });
   });
+
+  it("should return a 404 if the page is not found", () => {
+    return chai
+      .request(server)
+      .get("/sad")
+      .then(response => {
+        response.should.have.status(404);
+      });
+  });
+
+  
 });
